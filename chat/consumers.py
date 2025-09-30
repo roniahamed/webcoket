b4242ba973e92f647cc068a0df008bd2e815e2f3
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-09-30 06:59:00 +0600

    adding private group

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,10 @@
 import json 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+from .models import Message
+
+User = get_user_model
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -23,17 +28,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+    @database_sync_to_async
+    def save_message(self, author, room_name, content):
+        Message.objects.create(author = author, room_name = room_name, content = content)
+
+
     async def receive(self, text_data):
+
+        if self.scope['user'].is_authenticated:
+            return
+        
+
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        username = self.scope['user'].username if self.scope['user'].is_authenticated else 'AnonimousUser'
+        user = self.scope['user']
+
+        await self.save_message(author=user, room_name=self.room_name, content=message)
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type":"chat_message",
                 'message': message,
-                'username': username,
+                'username': user.username,
             }
         )
 
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Message
 
 def chat_index(request):
     return render(request, 'chat/index.html')
 
 def room(request, room_name):
-         return render(request, 'chat/room.html', {'room_name':room_name})
+         
+        message = Message.last_50_messages(room_name)
+        return render(request, 'chat/room.html', {'room_name':room_name})
 
 
 
